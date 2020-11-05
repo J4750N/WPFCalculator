8e6f473138778f47ed5a10e6a678cd0ca8664fe7
Jayson Taylor <1932645@champlaincollege.qc.ca>
2020-11-05 16:23:59 -0500

    Add project files.

---
+++ WPFCalculator/MainWindow.xaml.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static double lastNumber, result;
+
+        private static OperatorOptions selectedOperator;
+
+        private enum OperatorOptions
+        {
+            Addition, Subtraction, Multiplication, Division
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        
+            var theClicker = sender as Button;
+            String number = theClicker.Content.ToString();
+            if(Result.Content.ToString() == "0")
+            {
+                Result.Content = $"{number}";
+            }
+            else
+            {
+                Result.Content = $"{Result.Content}{number}";
+            }
+        
+
+        }
+          
+        private void Operation_Click(object sender, RoutedEventArgs e)
+        {
+            if(double.TryParse(Result.Content.ToString(), out lastNumber)){
+               Result.Content = "0" ; 
+            }
+            
+            if (sender == Add_Button)
+            {
+                selectedOperator = OperatorOptions.Addition;
+                
+            }
+            
+
+
+            if (sender == Subtract_Button)
+            {
+                selectedOperator = OperatorOptions.Subtraction;
+            }
+
+            if (sender == Multiply_Button)
+            {
+                selectedOperator = OperatorOptions.Multiplication;
+            }
+
+            if (sender == Divide_Button)
+            {
+                selectedOperator = OperatorOptions.Division;
+            }
+           
+        }
+
+
+
+        private void Equal_Click(object sender, RoutedEventArgs e)
+        {
+
+            double number;
+            if (double.TryParse(Result.Content.ToString(), out number))
+                
+            { 
+                
+                    switch (selectedOperator)
+                    {
+                        case OperatorOptions.Division:
+                            try
+                            {
+                                if (Result.Content.ToString() == "0")
+                                    throw new DivideByZeroException("Can't divide by 0");
+
+                                result = MathService.Divide(lastNumber, number);
+                            
+                            
+
+                        }
+                            catch (DivideByZeroException ex)
+                            {
+                                MessageBox.Show("Connot divide by 0", "Error!");
+                                lastNumber = 0;
+                            }
+                            break;
+
+                        case OperatorOptions.Multiplication:
+
+                            result = MathService.Multiply(lastNumber, number);
+                        
+
+                        break;
+
+                        case OperatorOptions.Addition:
+
+                            result = MathService.Add(lastNumber, number);
+                       
+                            break;
+
+                        case OperatorOptions.Subtraction:
+
+                            result = MathService.Subtract(lastNumber, number);
+                           
+                           
+                            break;
+                    }
+                
+            }
+        }
+
+        public void Percent_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            if (lastNumber == default(double))
+                {
+                    Result.Content = ((lastNumber / 100) * Double.Parse(Result.Content.ToString())).ToString();
+                }
+                else
+                {
+                    Result.Content = (Double.Parse(Result.Content.ToString()) * 0.01).ToString();
+                    lastNumber = Double.Parse(Result.Content.ToString());
+                }
+            
+            
+
+        }
+
+        public void AC_Click(object sender, RoutedEventArgs e)
+        {
+            Result.Content = "0";
+            
+        }
+
+        public void Negative_Click(object sender, RoutedEventArgs e)
+        {
+            Result.Content = double.Parse(Result.Content.ToString()) * -1;
+        }
+
+        public void Period_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Result.Content.ToString().Contains("."))
+                Result.Content += ".";
+        }
+            
+            public MainWindow()
+        {
+            InitializeComponent();
+
+            Equals_Button.Click += Equal_Click;
+            Percent_Button.Click += Percent_Click;
+            AC_Button.Click += AC_Click;
+            Negative_Button.Click += Negative_Click;
+            Period_Button.Click += Period_Click;
+        }
+
+    }
+}
---
+++ WPFCalculator/MathServices.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFCalculator
+{
+    static class MathService
+    {
+
+        public static double Add(double x1, double x2) => x1 + x2;
+        public static double Subtract(double x1, double x2) => x1 - x2;
+        public static double Multiply(double x1, double x2) => x1 * x2;
+        public static double Divide(double x1, double x2)
+        {
+
+            if (x1 == 0) // Have to force the exception cause it refuses to throw it by itself
+                throw new DivideByZeroException("Can't divide by zero");
+
+            return x1 / x2;
+        }
+        
+    }
+}
